812cb48ba2350e4ba713f8374008b2014aa697bf
nntto <nattou-king@hotmail.co.jp>
2022-10-20 15:19:30 +0900

    フォーマット

--- web/pages/api/last_connected_at/index.ts
+++ web/pages/api/last_connected_at/index.ts
@@ -1,20 +1,19 @@
-import { User, Device } from "@prisma/client";
-import type { NextApiRequest, NextApiResponse } from "next";
+import { User, Device, PrismaClient } from '@prisma/client'
+import type { NextApiRequest, NextApiResponse } from 'next'
 
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<
     (User & {
-      devices: Device[];
+      devices: Device[]
     })[]
-  >
+  >,
 ) {
   /* ユーザー，デバイステーブルを結合して返す */
   const users = await prisma.user.findMany({
     include: { devices: { include: { room: true } } },
-  });
-  res.status(200).json(users);
+  })
+  res.status(200).json(users)
 }
--- web/pages/api/rooms/index.ts
+++ web/pages/api/rooms/index.ts
@@ -1,14 +1,10 @@
-import { Room } from "@prisma/client";
-import type { NextApiRequest, NextApiResponse } from "next";
+import { Room, PrismaClient } from '@prisma/client'
+import type { NextApiRequest, NextApiResponse } from 'next'
 
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Room[]>
-) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Room[]>) {
   /* ユーザー，デバイステーブルを結合して返す */
-  const rooms = await prisma.room.findMany();
-  res.status(200).json(rooms);
+  const rooms = await prisma.room.findMany()
+  res.status(200).json(rooms)
 }
