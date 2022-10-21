dcc146826d7b64c4e65b15bab751e99e13175829
nntto <nattou-king@hotmail.co.jp>
2022-10-21 17:02:19 +0900

    feat: dB構造を更新
    WIP
    
    refactor: dBのIDをわかりやすくした

--- web/pages/api/rooms/index.ts
+++ web/pages/api/rooms/index.ts
@@ -1,10 +1,10 @@
-import { Room, PrismaClient } from '@prisma/client'
+import { Room, Network, PrismaClient } from '@prisma/client'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 const prisma = new PrismaClient()
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Room[]>) {
   /* ユーザー，デバイステーブルを結合して返す */
-  const rooms = await prisma.room.findMany()
+  const rooms = await prisma.room.findMany({ include: { Network: true } })
   res.status(200).json(rooms)
 }
---
+++ web/pages/test.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next'
+
+const Test: NextPage = () => {
+  return <p>Test</p>
+}
+
+export default Test
