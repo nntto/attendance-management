9a4c1de2d2861ebd3af6d69cccbc6f141faaa262
nntto <nattou-king@hotmail.co.jp>
2022-11-18 16:07:51 +0900

    feat: ユーザーの在室情報を取得するAPIを作成

--- web/pages/api/rooms/users.ts
+++ web/pages/api/rooms/users.ts
@@ -1,30 +1,73 @@
 import { PrismaClient, User, Comment, Nickname } from '@prisma/client'
 
 import type { NextApiRequest, NextApiResponse } from 'next'
+import inRoomTime from '../../../functions/inRoom'
 
 const prisma = new PrismaClient()
-
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { method } = req
 
   if (method === 'GET') {
-    const user = await prisma.user.findMany({
+    // 基準時刻inRoomTime()以降に接続された端末一覧を取得
+    const macAddressInRoom = await prisma.macAddress.findMany({
       where: {
-        
+        lastConnectedAt: {
+          gte: inRoomTime(),
+        },
+      },
+      orderBy: {
+        lastConnectedAt: 'desc', // 最終接続時刻が現在時刻から近い順
       },
       include: {
-        // Device: { include: { MacAddress: { include: { Network: { include: { Room: true } } } } } },
-        Comment: true,
-        Nickname: true,
-        
+        Device: true,
+        Network: true,
       },
     })
 
-    if (user === null) {
+    const users = await prisma.user.findMany({
+      where: {},
+      include: {
+        Comment: true,
+        Nickname: true,
+      },
+    })
+
+    const rooms = await prisma.room.findMany()
+    // 在室しているユーザーを格納する変数
+    const inRoom = rooms.map((room) => ({
+      ...room,
+      users: [] as (User & {
+        Comment: Comment[]
+        Nickname: Nickname[]
+      })[],
+    }))
+    // 在室していないユーザーを格納する変数
+    const outRoom = [] as (User & {
+      Comment: Comment[]
+      Nickname: Nickname[]
+    })[]
+
+    users.forEach((user) => {
+      const userMacAddress = macAddressInRoom.find(
+        (macAddress) => user.userId == macAddress.Device?.userId,
+      )
+      if (userMacAddress) {
+        const userRoom = inRoom.find((room) => room.roomId == userMacAddress.Network?.roomId)
+        if (userRoom) {
+          userRoom.users.push(user)
+          return
+        }
+        throw new Error('macAddressが部屋に紐づいていない')
+      }
+      outRoom.push(user)
+    })
+
+    //
+    if (users === null) {
       res.status(404).end('User NotFound')
     } else {
-      res.status(200).json(user)
+      res.status(200).json({ inRoom: inRoom, outRoom: outRoom })
     }
   } else {
     res.status(405).end('Method Not Allowed')
