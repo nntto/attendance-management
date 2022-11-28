290b8610ea207cc3791e4f561d5679acc62ffaf7
nntto <nattou-king@hotmail.co.jp>
2022-11-28 14:27:44 +0900

    feat: ユーザーテーブルをMUIに変更

---
+++ web/components/organisms/roomTable.tsx
@@ -0,0 +1,58 @@
+import Paper from '@mui/material/Paper'
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import * as React from 'react'
+import {
+  InroomAndOutroomUsersInRoomInnerAllOfUsersInner,
+  InroomAndOutroomUsersOutRoomInner,
+} from '../../types/typescript-axios'
+
+function createData(name: string, calories: number, fat: number, carbs: number, protein: number) {
+  return { name, calories, fat, carbs, protein }
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+]
+
+export default function RoomTable(props: {
+  users: InroomAndOutroomUsersInRoomInnerAllOfUsersInner[] & InroomAndOutroomUsersOutRoomInner[]
+}) {
+  const { users } = props
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label='simple table'>
+        <TableHead>
+          <TableRow>
+            <TableCell>学年</TableCell>
+            <TableCell align='right'>名前</TableCell>
+            <TableCell align='right'>ニックネーム</TableCell>
+            <TableCell align='right'>コメント</TableCell>
+            <TableCell align='right'>最終接続日時</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {users.map((row) => (
+            <TableRow key={row.name} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+              <TableCell component='th' scope='row'>
+                {row.grade}
+              </TableCell>
+              <TableCell align='right'>{row.name}</TableCell>
+              <TableCell align='right'>{row.nickname?.nickname}</TableCell>
+              <TableCell align='right'>{row.comment?.comment}</TableCell>
+              <TableCell align='right'>{row.lastConnectedAt}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
--- web/pages/rooms.tsx
+++ web/pages/rooms.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from 'next'
 import { useEffect, useState } from 'react'
+import RoomTable from '../components/organisms/roomTable'
 import { InroomAndOutroomUsers, RoomsApi } from '../types/typescript-axios'
 
 const Rooms: NextPage = () => {
@@ -16,24 +17,7 @@ const Rooms: NextPage = () => {
       {users.inRoom.map((room) => (
         <div key={room.roomId}>
           <h2 key={room.roomId}>{room.name}</h2>
-          <table>
-            <tbody>
-              <tr>
-                <th>学年</th>
-                <th>名前</th>
-                <th>ニックネーム</th>
-                <th>コメント</th>
-              </tr>
-              {room.users.map((user) => (
-                <tr key={user.userId}>
-                  <td>{user.grade}</td>
-                  <td>{user.name}</td>
-                  <td>{user.nickname?.nickname}</td>
-                  <td>{user.comment?.comment}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          <RoomTable users={room.users} />
         </div>
       ))}
       <h1>不在</h1>
