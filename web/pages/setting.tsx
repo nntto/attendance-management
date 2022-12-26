153c5229edffb4a88b55a514bcccf49b6d521b89
nntto <nattou-king@hotmail.co.jp>
2022-12-26 15:37:14 +0900

    feat: デバイス名変更機能実装

--- web/pages/setting.tsx
+++ web/pages/setting.tsx
@@ -1,3 +1,4 @@
+import { TextField } from '@mui/material'
 import { NextPage } from 'next'
 import { useEffect, useState } from 'react'
 import NetworkAndMacAddress from '../components/molecules/networkAndMacAddress'
@@ -5,12 +6,14 @@ import { RoomsApi, SettingApi, UserDevice } from '../types/typescript-axios'
 
 const Setting: NextPage = () => {
   const [userDevices, setUserDevices] = useState<UserDevice>({ rooms: [] })
+  const settingApi = new SettingApi()
 
+  // TODO: ログイン中のユーザーのデータを取得
   useEffect(() => {
-    const settingApi = new SettingApi()
     settingApi.getDevices('3').then((res) => {
       setUserDevices(res.data)
     })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return (
@@ -20,7 +23,15 @@ const Setting: NextPage = () => {
           <h1>部屋：{room.name}</h1>
           {room.devices.map((device) => (
             <div key={device.id}>
-              <h2>{device.name}</h2>
+              <TextField
+                onChange={(e) => {
+                  settingApi.putDevice(device.id, { name: e.target.value })
+                }}
+                variant='filled'
+                label='デバイス名'
+                id={String(device.id)}
+                defaultValue={device.name}
+              />
               {device.addresses.map((address) => (
                 <NetworkAndMacAddress
                   network={address.network}
