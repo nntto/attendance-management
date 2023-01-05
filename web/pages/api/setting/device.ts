7d5a6ea9be49f34fc592fb89405216a564ac2780
nntto <nattou-king@hotmail.co.jp>
2023-01-06 00:50:53 +0900

    feat: dELETEメソッドを実装

--- web/pages/api/setting/device.ts
+++ web/pages/api/setting/device.ts
@@ -6,19 +6,19 @@ const prisma = new PrismaClient()
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { method } = req
 
-  if (method === 'POST') {
-    try {
+  try {
+    if (method === 'POST') {
       const device = await prisma.device.create({
         data: req.body,
       })
       res.status(200).json(device)
-    } catch (e: unknown) {
-      if (e instanceof Error) {
-        console.log(e)
-        res.status(400).end(e.message)
-      }
+    } else {
+      res.status(405).end('Method Not Allowed')
     }
-  } else {
-    res.status(405).end('Method Not Allowed')
+  } catch (e: unknown) {
+    if (e instanceof Error) {
+      console.log(e)
+      res.status(400).end(e.message)
+    }
   }
 }
--- web/pages/api/setting/mac_address.ts
+++ web/pages/api/setting/mac_address.ts
@@ -6,19 +6,19 @@ const prisma = new PrismaClient()
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { method } = req
 
-  if (method === 'POST') {
-    try {
+  try {
+    if (method === 'POST') {
       const macAddress = await prisma.macAddress.create({
         data: req.body,
       })
       res.status(200).json(macAddress)
-    } catch (e: unknown) {
-      if (e instanceof Error) {
-        console.log(e)
-        res.status(400).end(e.message)
-      }
+    } else {
+      res.status(405).end('Method Not Allowed')
     }
-  } else {
-    res.status(405).end('Method Not Allowed')
+  } catch (e: unknown) {
+    if (e instanceof Error) {
+      console.log(e)
+      res.status(400).end(e.message)
+    }
   }
 }
