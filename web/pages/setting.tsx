23b793b1deea0d8d1a94beb091a22f0a32eef519
nntto <nattou-king@hotmail.co.jp>
2023-01-06 15:11:02 +0900

    feat: 29-frontsettingページでデバイス追加時にmac-address入力欄が表示されない問題を修正

--- web/pages/setting.tsx
+++ web/pages/setting.tsx
@@ -3,10 +3,11 @@ import cloneDeep from 'lodash/cloneDeep'
 import { NextPage } from 'next'
 import { useEffect, useState } from 'react'
 import RoomSetting from '../components/organisms/SettingRoomDevices'
-import { RoomsApi, SettingApi, UserDevice } from '../types/typescript-axios'
+import { Network, RoomsApi, SettingApi, UserDevice } from '../types/typescript-axios'
 
 const Setting: NextPage = () => {
   const [userDevices, setUserDevices] = useState<UserDevice>({ rooms: [] })
+  const [networks, setNetworks] = useState<Network[]>([])
   const settingApi = new SettingApi()
 
   // TODO: ログイン中のユーザーのデータを取得
@@ -14,6 +15,9 @@ const Setting: NextPage = () => {
   useEffect(() => {
     settingApi.getDevices(userId).then((res) => {
       setUserDevices(res.data)
+    })
+    settingApi.getNetworks().then((res) => {
+      setNetworks(res.data)
     })
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
@@ -23,7 +27,12 @@ const Setting: NextPage = () => {
       {userDevices.rooms.map((room) => (
         <div key={room.id}>
           <h1>部屋：{room.name}</h1>
-          <RoomSetting key={room.id} devices={room.devices} userId={userId}></RoomSetting>
+          <RoomSetting
+            key={room.id}
+            devices={room.devices}
+            userId={userId}
+            networks={networks}
+          ></RoomSetting>
         </div>
       ))}
     </div>
