a86e10d012a59292270b4e6e2a2a0358c86971dc
nntto <nattou-king@hotmail.co.jp>
2023-01-23 21:49:01 +0900

    feat: ヘッダーを追加

--- web/components/organisms/Header.tsx
+++ web/components/organisms/Header.tsx
@@ -1,18 +1,99 @@
-import { Box, Button } from '@mui/material'
-import { signIn, signOut, useSession } from 'next-auth/react'
+import { UrlObject } from 'url'
+import {
+  AppBar,
+  Avatar,
+  Box,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Tooltip,
+  Typography,
+} from '@mui/material'
+import { Container } from '@mui/system'
+import { signOut, useSession } from 'next-auth/react'
+import Link from 'next/link'
+import Router from 'next/router'
+import { useState } from 'react'
+
+const handler = (path: string | UrlObject) => {
+  Router.push(path)
+}
 
 export default function Header() {
   const { data, status } = useSession()
 
+  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null)
+
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget)
+  }
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null)
+  }
+
   if (status === 'loading') return <p>Loading...</p>
 
   return (
-    <Box>
-      {data ? (
-        <Button onClick={() => signOut()}>SignOut</Button>
-      ) : (
-        <Button onClick={() => signIn('google')}>SignIn</Button>
-      )}
-    </Box>
+    <AppBar position='static'>
+      <Container maxWidth='xl'>
+        <Toolbar disableGutters>
+          <Typography
+            variant='h6'
+            noWrap
+            component='a'
+            href='/rooms'
+            sx={{
+              mr: 2,
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            在室情報
+          </Typography>
+          <Box sx={{ flexGrow: 0 }}>
+            {data ? (
+              <>
+                <Tooltip title='Open settings'>
+                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                    <Avatar />
+                  </IconButton>
+                </Tooltip>
+                <Menu
+                  sx={{ mt: '45px' }}
+                  id='menu-appbar'
+                  anchorEl={anchorElUser}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  open={Boolean(anchorElUser)}
+                  onClose={handleCloseUserMenu}
+                >
+                  <MenuItem key={'setting'} onClick={() => handler('/setting')}>
+                    <Typography textAlign='center'>設定</Typography>
+                  </MenuItem>
+                  <MenuItem key={'signOut'} onClick={() => signOut()}>
+                    <Typography textAlign='center'>ログアウト</Typography>
+                  </MenuItem>
+                </Menu>
+              </>
+            ) : (
+              <></>
+            )}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   )
 }
