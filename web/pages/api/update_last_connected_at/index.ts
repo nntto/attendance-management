383c09e0eb24da0693676e00353581f1cfde5f2e
nntto <nattou-king@hotmail.co.jp>
2023-02-09 19:12:42 +0900

    feat: 指定したMACアドレスの最終接続日時を更新するAPI実装

---
+++ web/pages/api/update_last_connected_at/index.ts
@@ -0,0 +1,28 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from '../../../lib/prisma'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { method, body } = req
+  const { roomId, macAddresses }: { roomId: number; macAddresses: string[] } = JSON.parse(body)
+
+  if (method === 'PUT') {
+    try {
+      console.log(body)
+      await prisma.macAddress.updateMany({
+        where: {
+          address: { in: macAddresses },
+          Network: { Room: { id: roomId } },
+        },
+        data: { lastConnectedAt: new Date() },
+      })
+
+      res.status(200).end()
+    } catch (e: unknown) {
+      if (e instanceof Error) {
+        res.status(400).end(e.message)
+      }
+    }
+  } else {
+    res.status(405).end('Method Not Allowed')
+  }
+}
