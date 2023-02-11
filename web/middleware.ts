247cb3ee2ed42678dce1f8dce55bc3b65a30d057
nntto <nattou-king@hotmail.co.jp>
2023-02-11 12:10:03 +0900

    fix: mACアドレスの更新を行うAPIの認証を外す

--- web/middleware.ts
+++ web/middleware.ts
@@ -1,5 +1,5 @@
 export { default } from 'next-auth/middleware'
 
 export const config = {
-  matcher: ['/', '/((?!non-protected).*)'],
+  matcher: ['/', '/((?!non-protected|api/update_last_connected_at).*)'],
 }
